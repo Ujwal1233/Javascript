90e90a5495d78a3b600019b445fba86b8fa64e82
Ujwal D <ujwaldhananjaya@gmail.com>
2026-04-30 09:44:47 +0530

    Updated project files

---
+++ SpreadOperators/Applyingforobject.js
@@ -0,0 +1,15 @@
+var obj1={
+    name:"virat",
+    age:35,
+    city:"Delhi"
+}
+console.log(obj1)
+
+var obj2={
+    ...obj1,
+    id:18,
+    gender:"male",
+    gmail:"ujwaldhananjaya@gmail.com",
+    country:"India"
+}
+console.log(obj2)
