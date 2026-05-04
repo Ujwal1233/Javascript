42c209851f4502c706c4ba7b786420ec30ebdce0
Ujwal D <ujwaldhananjaya@gmail.com>
2026-05-04 09:15:19 +0530

    Updated project files

---
+++ Hoisting/Hoistingforvar.js
@@ -0,0 +1,8 @@
+console.log(a)//undefined
+var a=10
+console.log(a)
+
+var b//hoisted
+console.log(b)//undefined
+b=100
+console.log(b)
---
+++ Hoisting/letandconst.js
@@ -0,0 +1,8 @@
+let b=20
+console.log(b)
+// console.log(c)//reference error
+// let c=200
+let d//hoisted
+console.log(d)//undefined
+d=900
+console.log(d)
